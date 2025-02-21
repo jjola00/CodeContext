27d9533f4efc529a4b4364ae186ef55eac2430bb
Kami <jjolajitan2006@gmail.com>
2025-02-21 13:42:49 +0000

    updated database.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,10 @@
-# database.py
-import supabase
 import os
+import supabase
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db_client():
-    return supabase.create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    return supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
